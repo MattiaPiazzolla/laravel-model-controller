3311aeadb4bf98d666a2a843b557727ac4de9921
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-25 16:10:46 +0200

    [FIX] fixed Typo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\PageController as PageController;
+use App\Http\Controllers\PageController as PageController;
 
 /*
 |--------------------------------------------------------------------------
